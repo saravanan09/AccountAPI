7b3b7fad6a0a7238c4b16b04a10853f8c21a1f55
saravanan09 <vsaravanan1991@gmail.com>
2016-08-05 00:08:12 +0530

    Committed for Jenkins

--- src/test/java/accountWebAPI/AccountWebAPIRunnerTest.java
+++ src/test/java/accountWebAPI/AccountWebAPIRunnerTest.java
@@ -8,8 +8,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "classpath:features",
-		plugin = {"pretty", "html:target/cucumber-html-report","json:Folder_Name/cucumber.json" ,
-                           "junit:Folder_Name/cucumber.xml"},
+		plugin = {"pretty", "html:target/cucumber-html-report","json:target/cucumber.json" ,
+                           "junit:target/cucumber.xml"},
 		tags = {}
 		)
 
